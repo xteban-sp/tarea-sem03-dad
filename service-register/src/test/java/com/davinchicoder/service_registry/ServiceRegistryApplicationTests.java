49a19590148833991ca1aa18a1195298c300cfab
xteban-sp <xtebanflo@gmail.com>
2026-04-08 10:00:04 -0500

    1ERA VERSION OK?

---
+++ service-register/src/test/java/com/davinchicoder/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davinchicoder.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
